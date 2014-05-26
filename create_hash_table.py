aaf9cc1b5aab268a91051481ad2b4f0b4eb2977e
michael <michael.k.bilow@gmail.com>
2014-05-25 20:41:44 -0700

    More edits

--- create_hash_table.py
+++ create_hash_table.py
@@ -24,27 +24,28 @@ import os
 #         genome = ''.join([line.strip() for line in reference_file])
 #         return create_basic_hash(genome, key_length)
 
-def read_reference(reference_fn):
+def read_chromosome(reference_fn):
     with open(reference_fn, 'r') as reference_file:
         print reference_file.readline()
         chr_index = int(reference_file.readline().strip()[4:])
         return chr_index, ''.join([line.strip() for line in reference_file])
 
 
-def hash_reference(chromosome_id, chromosome_seq, key_length):
+def hash_chromosome(chromosome_id, chromosome_seq, key_length):
     genome_hash = defaultdict(list)
     for i in range(len(chromosome_seq) - key_length):
         key = chromosome_seq[i:i + key_length]
-        genome_hash[key].append((chromosome_id,i))
+        genome_hash[key].append((chromosome_id, i))
     return genome_hash
 
 
 if __name__ == "__main__":
     input_folder = './EE_genome'
-    ref = 'ref_genomeEExample.txt'
+    ref = 'ref/ref_genomeEExample.txt'
     ref_fn = os.path.join(input_folder, ref)
     key_size = 10
-    my_hash = hash_reference(ref_fn, key_size)
+    index, seq = read_chromosome(ref_fn)
+    my_hash = hash_chromosome(index, seq, key_size)
     k = 'AGATTTAAAC'
     print my_hash[k]
     k = 'CGGATATTAA'
